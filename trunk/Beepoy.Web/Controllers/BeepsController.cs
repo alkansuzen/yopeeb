6566a4db7e180b3a54ce449a27cc2d37a53a55e5
everaldo.beepoy@gmail.com <everaldo.beepoy@gmail.com@fff8c0c7-1f41-4ff8-a825-a029159ee25e>
2011-04-11 02:46:24 +0000

    Added Beepoy.SocialMedia.Twitter Project

---
+++ trunk/Beepoy.Web/Controllers/BeepsController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Beepoy.Web.Models;
+
+namespace Beepoy.Web.Controllers
+{
+    public class BeepsController : AppController
+    {
+        //
+        // GET: /Beeps/
+
+        public ActionResult Index(int Page = 0, int UserId = 0)
+        {
+            //Get Beeps from UserId
+            var beeps = Db.Beeps.Where(p => (p.UserId == UserId)).OrderBy(b => b.DateInsert).Skip(Page).Take(this.PageSize);
+
+            return View(beeps);
+        }
+
+        //
+        // GET: /Beeps/Details/5
+
+        public ActionResult Details(int id)
+        {
+            var beep = Db.Beeps.Find(id);
+
+            return View(beep);
+        }
+
+        //
+        // GET: /Beeps/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /Beeps/Create
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        
+        //
+        // GET: /Beeps/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Beeps/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+ 
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Beeps/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Beeps/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+ 
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Beeps/Beeps
+
+        public ActionResult Beeps(int Page = 0, int BeepIdFather = 0)
+        {
+            //Get Beeps from BeepIdFather
+            var beeps = Db.Beeps
+                .Where(b => (b.BeepIdFather == BeepIdFather))
+                .OrderByDescending(b => b.DateInsert)
+                .Skip(Page)
+                .Take(this.PageSize);
+
+            return View(beeps);
+        }
+    }
+}
---
+++ trunk/Beepoy.Web/Controllers/EventsController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Beepoy.Web.Models;
+
+namespace Beepoy.Web.Controllers
+{
+    public class EventsController : AppController
+    {
+        //
+        // GET: /Events/
+
+        public ActionResult Index(int Page)
+        {
+            var events = Db.Events.OrderBy(e => e.DateInsert).Skip(Page).Take(this.PageSize);
+
+            return View();
+        }
+
+        //
+        // GET: /Events/Details/5
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /Events/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /Events/Create
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        
+        //
+        // GET: /Events/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Events/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+ 
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Events/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Events/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+ 
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Events/Beeps
+
+        public ActionResult Beeps(int Page = 0, int EventId = 0)
+        {
+            //Get Beeps from EventId
+            var beeps = Db.Beeps
+                .Where(b => (b.EventId == EventId))
+                .OrderByDescending(b => b.DateInsert)
+                .Skip(Page)
+                .Take(this.PageSize);
+
+            return View(beeps);
+        }
+    }
+}
