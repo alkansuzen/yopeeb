ba5f636a378cff48923e5120b8bce0bb4ebd338d
camilo.beepoy@gmail.com <camilo.beepoy@gmail.com@fff8c0c7-1f41-4ff8-a825-a029159ee25e>
2011-05-27 22:45:47 +0000

    - Teste NHibernate

---
+++ trunk/TesteNhibernate/TesteNhibernate/Models/Repositories/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace TesteNhibernate.Models
+{
+    public class DataContext:IDataContext
+    {
+      
+        public IUserRepository Users
+        {
+            get
+            {
+                return new UserRepository();
+            }
+        }
+    }
+}
---
+++ trunk/TesteNhibernate/TesteNhibernate/Models/Repositories/UserRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TesteNhibernate.Models
+{
+    public class UserRepository : NHibernateRepository<User>, IUserRepository
+    {
+    }
+}
