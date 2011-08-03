bf88bfce4c67269b28927f8cec294fdaf9077e77
everaldo.beepoy@gmail.com <everaldo.beepoy@gmail.com@fff8c0c7-1f41-4ff8-a825-a029159ee25e>
2011-08-03 02:29:15 +0000

    tweetandp

--- trunk/Beepoy.SocialMedia.Twitter/Authentication.cs
+++ trunk/Beepoy.SocialMedia.Twitter/Authentication.cs
@@ -4,61 +4,48 @@ using System.Linq;
 using System.Text;
 using System.Configuration;
 using TweetSharp;
+using TweetSharp.Model;
 
 namespace Beepoy.SocialMedia.Twitter
 {
     public class Authentication
     {
-        public static void AuthorizeApp()
+        TwitterService twitterService;
+        OAuthRequestToken requestToken;
+
+        public string AuthorizeApp()
         {
-            // OAuth Access Token Exchange
-            TwitterService service = new TwitterService("xDGISMTarPrTJj7AzYDSqA", "p1F9oVPP2RNYGMN0siZVgd4MaJ9KmfOQZeeommsv6Ng", "19319611-Qse2lpCEqmbbE534RADJssSZ34F7KMgGKgXRUeAqY", "hmSV3SrY9cZbGp8PmF1Czi24WM13GOEovRUZpndbM5w");
-            OAuthRequestToken unauthorizedToken = service.GetRequestToken();
-            string url = service.GetAuthorizationUri(unauthorizedToken).AbsolutePath;
+            TwitterClientInfo twitterClientInfo = new TwitterClientInfo();
+            twitterClientInfo.ConsumerKey = ConfigurationManager.AppSettings["ConsumerKey"]; //Read ConsumerKey out of the app.config
+            twitterClientInfo.ConsumerSecret = ConfigurationManager.AppSettings["ConsumerSecret"]; //Read the ConsumerSecret out the app.config
+            
+            string accessToken = ConfigurationManager.AppSettings["AccessToken"];
+            //string accessTokenSecret = ConfigurationManager.AppSettings["AccessTokenSecret"];
+
+            twitterService = new TwitterService(twitterClientInfo);
+
+            //Now we need the Token and TokenSecret
+            //Firstly we need the RequestToken and the AuthorisationUrl
+            requestToken = twitterService.GetRequestToken();
+            string authUrl = twitterService.GetAuthorizationUri(requestToken).AbsoluteUri;
+
+            return authUrl;
         }
 
-        public static void Login()
+        public bool Authenticate(string pin)
         {
-            // OAuth Access Token Exchange
-            TwitterService service = new TwitterService("xDGISMTarPrTJj7AzYDSqA", "p1F9oVPP2RNYGMN0siZVgd4MaJ9KmfOQZeeommsv6Ng", "19319611-Qse2lpCEqmbbE534RADJssSZ34F7KMgGKgXRUeAqY", "hmSV3SrY9cZbGp8PmF1Czi24WM13GOEovRUZpndbM5w");
-            OAuthRequestToken unauthorizedToken = service.GetRequestToken();
-            string url = service.GetAuthorizationUri(unauthorizedToken).AbsolutePath;
+            OAuthAccessToken accessToken = twitterService.GetAccessToken(requestToken, pin);
 
-            string pin = "7990245";
-            OAuthAccessToken authToken = service.GetAccessToken(unauthorizedToken, pin);
+            twitterService.AuthenticateWith(accessToken.Token, accessToken.TokenSecret);
 
-            service.AuthenticateWith("xDGISMTarPrTJj7AzYDSqA", "p1F9oVPP2RNYGMN0siZVgd4MaJ9KmfOQZeeommsv6Ng", "19319611-Qse2lpCEqmbbE534RADJssSZ34F7KMgGKgXRUeAqY", "hmSV3SrY9cZbGp8PmF1Czi24WM13GOEovRUZpndbM5w");
-            if (service.Response.InnerException == null)
-            {
-                TwitterStatus status = service.SendTweet("Bach");
-            }
-            
-            
-//using TweetSharp.Model;
-//using TweetSharp.Twitter.Extensions;
-//using TweetSharp.Twitter.Fluent;
-//using TweetSharp.Twitter.Model;
-//using TweetSharp.Twitter.Service;
- 
-////generate the token and url that will we need
-//OAuthToken unauthorizedToken = service.GetRequestToken(twitterConsumerKey, twitterConsumerSecret);
-//string url = service.GetAuthorizationUrl(unauthorizedToken);
- 
-////TODO: get the pin from the user
- 
-////once you have the pin from the user, build your authorized token
-//OAuthToken authToken = service.GetAccessToken(twitterConsumerKey, twitterConsumerSecret, unauthToken, pin);
- 
-////store the authToken.Token, and the authToken.TokenSecret
- 
-////make an authenticated call
-//var service = new TwitterService();
-//service.AuthenticateWith(twitterConsumerKey, twitterConsumerSecret, syndicationService.OAuthToken, syndicationService.OAuthTokenSecret);
-//if (service.Error == null)
-//{
-//TwitterStatus status = service.SendTweet(value);
-//}
+            if (twitterService.Response.InnerException != null)
+                return false;
+            return true;
+        }
 
+        public TwitterStatus Tweet(string message)
+        {
+            return twitterService.SendTweet(message);
         }
     }
 }
--- trunk/Beepoy.SocialMedia.Twitter/TwitterInfo.cs
+++ trunk/Beepoy.SocialMedia.Twitter/TwitterInfo.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Configuration;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,29 +9,17 @@ namespace Beepoy.SocialMedia.Twitter
 {
     public class TwitterInfo
     {
-        private readonly string _hero;
-        private readonly string _consumerKey;
-        private readonly string _consumerSecret;
-        private readonly string _accessToken;
-        private readonly string _accessTokenSecret;
+        private readonly string ConsumerKey;
+        private readonly string ConsumerSecret;
+        private readonly string AccessToken;
+        private readonly string AccessTokenSecret;
 
         public TwitterInfo()
         {
-            //_hero = ConfigurationManager.AppSettings["Hero"];
-            //_consumerKey = ConfigurationManager.AppSettings["ConsumerKey"];
-            //_consumerSecret = ConfigurationManager.AppSettings["ConsumerSecret"];
-            //_accessToken = ConfigurationManager.AppSettings["AccessToken"];
-            //_accessTokenSecret = ConfigurationManager.AppSettings["AccessTokenSecret"];
-        }
-
-        public static void Hello()
-        {
-            TwitterService service = new TwitterService();
-            IEnumerable<TwitterStatus> tweets = service.ListTweetsOnPublicTimeline();
-            foreach (var tweet in tweets)
-            {
-                Console.WriteLine("{0} says '{1}'", tweet.User.ScreenName, tweet.Text);
-            }
+            ConsumerKey = ConfigurationManager.AppSettings["ConsumerKey"];
+            ConsumerSecret = ConfigurationManager.AppSettings["ConsumerSecret"];
+            AccessToken = ConfigurationManager.AppSettings["AccessToken"];
+            AccessTokenSecret = ConfigurationManager.AppSettings["AccessTokenSecret"];
         }
     }
 }
